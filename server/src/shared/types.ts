cc7fa770bd80186fdc698bfa1db0ef8548f10fb2
Crayee <till.gohl@gmx.de>
2024-03-29 15:00:00 +0000

    fix username validation

--- server/src/shared/types.ts
+++ server/src/shared/types.ts
@@ -7,7 +7,13 @@ import { z } from 'zod'
 const userSchema = createSelectSchema(users)
 export type User = z.infer<typeof userSchema>
 
-export const createUserSchema = createInsertSchema(users)
+export const createUserSchema = createInsertSchema(users, {
+    username: z
+        .string()
+        .max(50, "Username can't be longer than 50 characters")
+        .min(3, 'Username must be at least 3 characters long')
+        .regex(/^[a-zA-Z0-9_]*$/, 'Username can only contain letters, numbers, and underscores'),
+})
 export type CreateUser = z.infer<typeof createUserSchema>
 
 export const getUserSchema = createSelectSchema(users).pick({ username: true })
