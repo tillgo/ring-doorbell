f6f6bc15226c8cc36352c1cf277e8357b8fa3e93
tillgo <till.gohl@gmx.de>
2024-04-03 11:22:52 +0100

    change dark mode toggle to just two modes

--- web/src/components/mode-toggle.tsx
+++ web/src/components/mode-toggle.tsx
@@ -1,25 +1,30 @@
-import { Moon, Sun, SunMoon } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { useTheme } from '@/common/hooks/useTheme'
 
 export function ModeToggle() {
     const { setTheme, theme } = useTheme()
 
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+
     const handleClick = () => {
-        if (theme === 'light') {
-            setTheme('dark')
+        if (theme === 'system') {
+            setTheme(systemTheme === 'dark' ? 'light' : 'dark')
         } else if (theme === 'dark') {
-            setTheme('system')
-        } else {
             setTheme('light')
+        } else {
+            setTheme('dark')
         }
     }
 
     return (
         <Button variant="outline" size="icon" onClick={handleClick}>
-            {theme === 'light' && <Sun className={'h-4 w-4'} />}
-            {theme === 'dark' && <Moon className={'h-4 w-4'} />}
-            {theme === 'system' && <SunMoon className={'h-4 w-4'} />}
+            {(theme === 'light' || (theme === 'system' && systemTheme === 'light')) && (
+                <Sun className={'h-4 w-4'} />
+            )}
+            {(theme === 'dark' || (theme === 'system' && systemTheme === 'dark')) && (
+                <Moon className={'h-4 w-4'} />
+            )}
         </Button>
     )
 }
