77e7ae7f907a33966d2a7ad5dd11e843eff59f74
Michael Delser <michadelser@gmail.com>
2024-05-28 13:00:27 +0100

    Update opencv test file

--- bell-client/opencv.py
+++ bell-client/opencv.py
@@ -1,25 +1,29 @@
-# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-# initialize the camera and grab a reference to the raw camera capture
+
+#Instantiate and configure picamera
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
+raw_capture = PiRGBArray(camera, size=(640, 480))
+
+#let camera module warm up
 time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+
+# define an OpenCV window to display  video
+cv2.namedWindow("Frame")
+
+#Capture continuous frames to access video
+for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+    image = frame.array
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    raw_capture.truncate(0)
+    #if 'q' is pressed, close OpenCV window and end video
+    if key != ord('q'):
+        pass
+    else:
+        cv2.destroyAllWindows()
+        break
