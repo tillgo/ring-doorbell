9b89035c103f069edc610e26e656e9f3c5e2f587
Siggi <simeon-holzapfel@gmx.de>
2024-05-28 13:43:10 +0200

    test

--- bell-client/main.py
+++ bell-client/main.py
@@ -3,6 +3,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWid
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QPixmap
 from picamera_stream import PiCameraStream
+from utils.camera_thread import MyThread
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,27 +15,24 @@ class MainWindow(QMainWindow):
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         layout = QVBoxLayout(self.central_widget)
-        
+
         self.video_label = QLabel(self)
         layout.addWidget(self.video_label)
 
-        self.camera_stream = PiCameraStream()
+        self.camera_thread = MyThread()
+        self.camera_thread.frame_signal.connect(self.update_frame)
+        self.camera_thread.start()
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-        self.timer.start(50)  # Update every 50 milliseconds
+    async def update_frame(self, image):
+        self.video_label.setPixmap(QPixmap.fromImage(image))
 
-    async def update_frame(self):
-        frame = await self.camera_stream.recv()
-        if frame is not None:
-            self.video_label.setPixmap(QPixmap.fromImage(frame))
-            self.video_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
---
+++ bell-client/utils/camera_thread.py
@@ -0,0 +1,26 @@
+# https://medium.com/@ilias.info.tel/display-opencv-camera-on-a-pyqt-app-4465398546f7
+import cv2
+import imutils
+from PyQt6.QtCore import QThread, pyqtSignal as Signal
+from PyQt6.QtGui import QImage
+
+
+def cvimage_to_label(image):
+    image = imutils.resize(image, width=640)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = QImage(image,
+                   image.shape[1],
+                   image.shape[0],
+                   QImage.Format_RGB888)
+    return image
+
+
+class MyThread(QThread):
+    frame_signal = Signal(QImage)
+
+    def run(self):
+        self.cap = cv2.VideoCapture(0)
+        while self.cap.isOpened():
+            _, frame = self.cap.read()
+            frame = cvimage_to_label(frame)
+            self.frame_signal.emit(frame)
