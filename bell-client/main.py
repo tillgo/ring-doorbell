9f9183bce02b5239f2830f6d0b916690c997eff5
Siggi <simeon-holzapfel@gmx.de>
2024-05-28 14:04:11 +0200

    test camera

--- bell-client/main.py
+++ bell-client/main.py
@@ -3,36 +3,39 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWid
 from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QPixmap
 from picamera_stream import PiCameraStream
-from utils.camera_thread import MyThread
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Raspberry Pi Camera Stream")
-        self.setGeometry(0, 0, 480, 250)
+        self.setGeometry(100, 100, 480, 250)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         layout = QVBoxLayout(self.central_widget)
-
+        
         self.video_label = QLabel(self)
         layout.addWidget(self.video_label)
 
-        self.camera_thread = MyThread()
-        self.camera_thread.frame_signal.connect(self.update_frame)
-        self.camera_thread.start()
+        self.camera_stream = PiCameraStream()
 
-    async def update_frame(self, image):
-        self.video_label.setPixmap(QPixmap.fromImage(image))
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(50)  # Update every 50 milliseconds
 
+    async def update_frame(self):
+        frame = await self.camera_stream.recv()
+        if frame is not None:
+            pixmap = QPixmap()
+            pixmap.loadFromData(frame)
+            self.video_label.setPixmap(pixmap)
+            self.video_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
