4ce557387aa8cc4c6eb51e6b2d02f9486bd48e13
Siggi <simeon-holzapfel@gmx.de>
2024-05-28 15:27:42 +0200

    test camera

--- bell-client/utils/camera_thread.py
+++ bell-client/utils/camera_thread.py
@@ -1,27 +1,28 @@
+import fractions
+import time
+
 import cv2
 from PyQt6.QtCore import QThread, Qt, pyqtSignal as Signal
 from PyQt6.QtGui import QImage
 from PyQt6.QtWidgets import QWidget, QLabel, QApplication
+from picamera2 import Picamera2, MappedArray
 
 
 class CameraThread(QThread):
     frame_available = Signal(QImage)
 
     def __init__(self, capture_device=0, parent=None):
-        super().__init__(parent)
-        self.cap = cv2.VideoCapture(capture_device)
+        super().__init__()
+        self.camera = Picamera2()
+        self.camera.configure(self.camera.create_preview_configuration(main={"format": "RGB888"}))
+        self.camera.start()
 
     def run(self):
         while self.isRunning():
-            ret, frame = self.cap.read()
+            frame = self.camera.capture_array()
+            video_frame = VideoFrame.from_ndarray(frame, format="rgb24")
+            video_frame.pts = int(time.time() * 1000000)
+            video_frame.time_base = fractions.Fraction(1, 1000000)
             print("At least method works")
             print(frame)
-            if ret:
-                print("There is a frame")
-                print(frame)
-                # Convert BGR to RGB for PyQt
-                rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                height, width, channels = rgb_image.shape
-                bytes_per_line = channels * width
-                qt_image = QImage(rgb_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
-                self.frame_available.emit(qt_image)
+            self.frame_available.emit(video_frame)
