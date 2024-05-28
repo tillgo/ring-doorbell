04b1e1dbf5cf20801892a0ef2f4fc5c902142e79
Michael Delser <michadelser@gmail.com>
2024-05-28 13:15:23 +0100

    Update opencv test file

--- bell-client/opencv.py
+++ bell-client/opencv.py
@@ -1,13 +1,13 @@
 from picamera.array import PiRGBArray
-from picamera import PiCamera
+from picamera2 import PiCamera2
 import time
 import cv2
 
 #Instantiate and configure picamera
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-raw_capture = PiRGBArray(camera, size=(640, 480))
+camera2 = PiCamera2()
+camera2.resolution = (640, 480)
+camera2.framerate = 32
+raw_capture = PiRGBArray(camera2, size=(640, 480))
 
 #let camera module warm up
 time.sleep(0.1)
@@ -16,7 +16,7 @@ time.sleep(0.1)
 cv2.namedWindow("Frame")
 
 #Capture continuous frames to access video
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+for frame in camera2.capture_continuous(raw_capture, format="bgr", use_video_port=True):
     image = frame.array
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
