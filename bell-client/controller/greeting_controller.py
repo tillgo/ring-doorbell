967aee5b0a1940f0f5243ef499c17fb2e0894683
Siggi <simeon-holzapfel@gmx.de>
2024-05-29 12:45:38 +0200

    test calling frontend fix

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -7,8 +7,11 @@ class GreetingController:
         self.ui = ui
         self.call_user_controller = CallUserController(ui)
         #UserId of user Till for testing (password testtest)
-        self.ui.call_user_btn.clicked.connect(self.call_user_controller.call_user("4dc66649-3b1d-426f-89f0-3df95fd02a3c"))
+        self.ui.call_user_btn.clicked.connect(self.handle_call_user)
 
     def open_greeting_page(self, username: str):
         self.ui.uid_label.setText(username)
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.greeting_page)
+
+    def handle_call_user(self):
+        self.call_user_controller.call_user("4dc66649-3b1d-426f-89f0-3df95fd02a3c")
