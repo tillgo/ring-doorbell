287f7d44705ccc432be2b5c19123e85d933fbbd6
Michael Delser <michadelser@gmail.com>
2024-05-30 18:01:19 +0100

    picamera

--- bell-client/picamera_stream.py
+++ bell-client/picamera_stream.py
@@ -2,11 +2,14 @@ from PyQt6.QtWidgets import QApplication
 from picamera2.previews.qt import QGlPicamera2
 from picamera2 import Picamera2
 
+app = QApplication([])  # Create the QApplication instance
+
 picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration())
-app = QApplication([])
+
 qpicamera2 = QGlPicamera2(picam2, width=800, height=600, keep_ar=False)
 qpicamera2.setWindowTitle("Qt Picamera2 App")
+
 picam2.start()
 qpicamera2.show()
 app.exec()
