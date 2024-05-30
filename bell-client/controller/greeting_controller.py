6d158a8333931321c1e7db03d03d8027a877007b
Siggi <simeon-holzapfel@gmx.de>
2024-05-30 17:48:08 +0200

    test calling frontend fix

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -17,4 +17,6 @@ class GreetingController:
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.greeting_page)
 
     def handle_call_user(self):
-        self.call_user_controller.call_user("4dc66649-3b1d-426f-89f0-3df95fd02a3c")
+      self.call_user_controller.call_user("4dc66649-3b1d-426f-89f0-3df95fd02a3c")
+
+
