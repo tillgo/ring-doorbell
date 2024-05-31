2ed1c1ca7d274e3a39cc12b03ee971848d5f04fe
Siggi <simeon-holzapfel@gmx.de>
2024-05-31 19:51:43 +0200

    test usb mic working

--- bell-client/utils/piaudiotrack.py
+++ bell-client/utils/piaudiotrack.py
@@ -3,6 +3,7 @@ import wave
 import pyaudio
 from aiortc import MediaStreamTrack
 
+# Dont worry about the many errors. This are now errors but just warnings. Everything is working fine.
 
 # Device Index of USB-Mic: hw: 2.0 = index 3
 # If not found run the following and look for USB PnP Sound Device in the output:
@@ -11,14 +12,27 @@ from aiortc import MediaStreamTrack
 # >>> for ii in range(p.get_device_count()):
 # >>>     print(p.get_device_info_by_index(ii).get('name'))
 
+form_1 = pyaudio.paInt16  # 16-bit resolution
+chans = 1  # 1 channel
+samp_rate = 44100  # 44.1kHz sampling rate
+chunk = 4096  # 2^12 samples for buffer
+record_secs = 6  # seconds to record
+dev_index = 3  # device index found by p.get_device_info_by_index(ii)
+wav_output_filename = 'test1.wav'  # name of .wav file
+
+
 class PiAudioTrack(MediaStreamTrack):
     kind = "audio"
 
     def __init__(self):
         super().__init__()
+        self.audio = pyaudio.PyAudio()
+        stream = audio.open(format=form_1, rate=samp_rate, channels=chans,
+                            input=True,
+                            frames_per_buffer=chunk)
 
     async def recv(self):
-        pass
+        return stream.read(chunk)
 
 
 if __name__ == '__main__':
@@ -34,7 +48,7 @@ if __name__ == '__main__':
 
     # create pyaudio stream
     stream = audio.open(format=form_1, rate=samp_rate, channels=chans,
-                        input=True,
+                        output_device_index=dev_index, input=True,
                         frames_per_buffer=chunk)
     print("recording")
     frames = []
