322d0c8cce8e8afab527433b3e71d787657d34e8
Siggi <simeon-holzapfel@gmx.de>
2024-05-31 22:15:00 +0200

    test usb mic working

--- bell-client/controller/call_user_controller.py
+++ bell-client/controller/call_user_controller.py
@@ -33,7 +33,7 @@ class CallUserController:
         peer = RTCPeerConnection(RTCConfiguration(iceServers=[RTCIceServer(urls="stun:stun1.l.google.com:19302"),
                                                               RTCIceServer(urls="stun:stun2.l.google.com:19302")]))
 
-        peer.on('track', lambda event: print("Track received"))
+        peer.on('track', lambda event: print("Track received " + event.track))
 
         # add video
         camTrack = PiCameraTrack()
