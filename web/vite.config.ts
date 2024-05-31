ae8dd48aacaaf167a8bb946460af4e82bab24b9d
tillgo <till.gohl@gmx.de>
2024-05-31 18:15:08 +0200

    test

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
                 name: 'Ring Doorbell',
                 short_name: 'Ring Doorbell',
                 description: 'Ring Doorbell app',
-                theme_color: '???',
+                theme_color: '#131313',
                 icons: [
                     {
                         src: 'icon-192.png',
