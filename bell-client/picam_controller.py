02cbfeb8183734979c141b6056c52ebbbb502bf3
Michael Delser <michadelser@gmail.com>
2024-05-31 13:09:17 +0100

    picam_controller

--- bell-client/picam_controller.py
+++ bell-client/picam_controller.py
@@ -2,12 +2,20 @@ import time
 import av
 from fractions import Fraction
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
 from aiortc.contrib.media import MediaStreamTrack
 
 cam = Picamera2()
+output = FfmpegOutput('test.mp4', audio=True)
 cam.configure(cam.create_video_configuration())
+encoder = H264Encoder(1000000)
+ffmpeg = FfmpegOutput(audio=True)
+encoder.output = [ffmpeg]
+cam.encoders = encoder
 cam.start()
+cam.start_encoder()
 
 class PiCameraTrack(MediaStreamTrack):
     kind = "video"
