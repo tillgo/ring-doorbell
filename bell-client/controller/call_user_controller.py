08adea281a853569c7ca669150cf12bc959fe0ae
Siggi <simeon-holzapfel@gmx.de>
2024-06-01 14:39:34 +0200

    Reference RTCPeer from outside

--- bell-client/controller/call_user_controller.py
+++ bell-client/controller/call_user_controller.py
@@ -58,6 +58,7 @@ class CallUserController:
             await asyncio.sleep(3)
             print("State: " + self.peer.connectionState)
             print("CPU %" + str(psutil.cpu_percent()))
+            print("MEMORY" + str(psutil.virtual_memory().percent) + "%")
             if (self.peer.connectionState == "failed" or self.peer.connectionState == "disconnected"
                     or self.peer.connectionState == "closed"):
                 print("Exiting now")
