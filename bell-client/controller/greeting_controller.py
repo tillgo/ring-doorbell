e45c7f78553c9a10f5a7ed7842803aa36285fc9d
Siggi <simeon-holzapfel@gmx.de>
2024-06-01 15:56:26 +0200

    Reference RTCPeer from outside

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -17,7 +17,7 @@ class GreetingController:
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.greeting_page)
 
     def handle_call_user(self):
-        #UserId of user Siggi for testing (password TestTest) (productive)
-      self.call_user_controller.call_user("142b3e7f-1562-4335-9653-d98eac0c6f73")
+        #UserId of user Siggi1 for testing (password testtest)
+      self.call_user_controller.call_user("b9b5e449-d090-4ca3-8b4f-b0585ad908cd")
 
 
