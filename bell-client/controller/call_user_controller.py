c4ec1317712c8f5336d21ef6fcfcb83129a2e30c
Siggi <simeon-holzapfel@gmx.de>
2024-06-01 11:01:02 +0200

    Reference RTCPeer from outside

--- bell-client/controller/call_user_controller.py
+++ bell-client/controller/call_user_controller.py
@@ -6,7 +6,6 @@ from aiortc.contrib.media import MediaPlayer
 
 from connectionClients.socket_client import SocketClient
 from utils.piaudiotrack import PiAudioTrack
-from utils.picameratrack import PiCameraTrack
 
 
 class CallUserController:
