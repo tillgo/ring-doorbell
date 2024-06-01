93f298b481f7614ccf832684a3b6cd9a0b99d880
Siggi <simeon-holzapfel@gmx.de>
2024-06-01 13:42:50 +0200

    Reference RTCPeer from outside

--- bell-client/utils/picameratrack.py
+++ bell-client/utils/picameratrack.py
@@ -1,6 +1,4 @@
 import time
-from multiprocessing import Process
-
 import av
 from fractions import Fraction
 from picamera2 import Picamera2
@@ -11,6 +9,7 @@ from aiortc.contrib.media import MediaStreamTrack
 
 output = FfmpegOutput('test.mp4', audio=True)
 
+
 #encoder = H264Encoder(1000000)
 #ffmpeg = FfmpegOutput(audio=True)
 #encoder.output = [ffmpeg]
@@ -19,37 +18,19 @@ output = FfmpegOutput('test.mp4', audio=True)
 
 #cam.start_encoder()
 
-frames = []
-
-
-def record_frames():
-    cam = Picamera2()
-    cam.configure(cam.create_video_configuration())
-    cam.start()
-    while True:
-        print("getting frame")
-        img = cam.capture_array()
-        pts = time.time() * 60
-        new_frame = av.VideoFrame.from_ndarray(img, format='rgba')
-        new_frame.pts = int(pts)
-        new_frame.time_base = Fraction(1, 60)
-        frames.append(new_frame)
-
-
 class PiCameraTrack(MediaStreamTrack):
     kind = "video"
 
     def __init__(self):
         super().__init__()
-        video_thread = Process(target=record_frames)
-        video_thread.start()
+        self.cam = Picamera2()
+        self.cam.configure(self.cam.create_video_configuration())
+        self.cam.start()
 
     async def recv(self):
-        while True:
-            print("try to get frame")
-            if len(frames) > 0:
-                new_frame = frames.pop()
-                print("returning frane")
-                print(new_frame)
-                return new_frame
-
+        img = self.cam.capture_array()
+        pts = time.time() * 60
+        new_frame = av.VideoFrame.from_ndarray(img, format='rgba')
+        new_frame.pts = int(pts)
+        new_frame.time_base = Fraction(1, 60)
+        return new_frame
