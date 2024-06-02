b6083a3c8baf5103c18424f7889ce076b064b4d5
Michael Delser <michadelser@gmail.com>
2024-06-02 19:54:45 +0100

    clear userList before inserting users

--- bell-client/controller/after_call_controller.py
+++ bell-client/controller/after_call_controller.py
@@ -19,14 +19,14 @@ class AfterCallController:
     def __init__(self, end_type: str, ui, main_controller):
         self.message_type = end_type
         self.ui = ui
+        QtCore.QCoreApplication.quit()
+        status = QtCore.QProcess.startDetached(sys.executable, sys.argv)
+        print(status)
         self.main_controller = main_controller
         self.ui.call_end_message.setText(messages[end_type])
         self.countdown: int = 3
         self.ui.countdown.setText(str(self.countdown))
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.after_call_page)
-        QtCore.QCoreApplication.quit()
-        status = QtCore.QProcess.startDetached(sys.executable, sys.argv)
-        print(status)
         self.main_controller.start_app()
 
 
