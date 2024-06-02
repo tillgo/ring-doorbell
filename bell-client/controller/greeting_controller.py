ad5847e680cf45ef8ddfda5fe2306d532c7eecb5
Michael Delser <michadelser@gmail.com>
2024-06-02 11:24:36 +0100

    ring with nfcCardID, connect before posting

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -15,6 +15,7 @@ class GreetingController:
 
     def open_greeting_page(self, nfcCardID: str):
         httpClient = HttpClient()
+        httpClient.connect()
         httpClient.get_visitor(nfcCardID)
         self.ui.uid_label.setText(nfcCardID)
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.greeting_page)
