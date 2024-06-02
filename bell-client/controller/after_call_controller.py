335d0b0de61804d2d47a02701cacd91eb8f33849
Michael Delser <michadelser@gmail.com>
2024-06-02 19:52:25 +0100

    clear userList before inserting users

--- bell-client/controller/after_call_controller.py
+++ bell-client/controller/after_call_controller.py
@@ -1,5 +1,7 @@
 import asyncio
+import sys
 
+from PyQt6.uic.properties import QtCore
 
 messages = {
     'failed': 'Call failed (User not online)',
@@ -22,6 +24,9 @@ class AfterCallController:
         self.countdown: int = 3
         self.ui.countdown.setText(str(self.countdown))
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.after_call_page)
+        QtCore.QCoreApplication.quit()
+        status = QtCore.QProcess.startDetached(sys.executable, sys.argv)
+        print(status)
         self.main_controller.start_app()
 
 
