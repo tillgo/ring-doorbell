7734158d0cb55c2496d98751a301c95f30f877d5
Siggi <simeon-holzapfel@gmx.de>
2024-06-02 14:35:44 +0200

    make changes to token adapter

--- bell-client/utils/AudioPlayer.py
+++ bell-client/utils/AudioPlayer.py
@@ -1,5 +1,8 @@
+import asyncio
+
 import av
 import pyaudio
+from aiortc.contrib.media import MediaRecorder
 from aiortc.rtcrtpreceiver import RemoteStreamTrack
 
 
@@ -29,12 +32,9 @@ class AudioPlayer:
         self.track = track
 
     async def play(self):
-        audio = pyaudio.PyAudio()
-        stream = audio.open(format=pyaudio.paInt16,
-                            channels=2,
-                            rate=44100,
-                            output=True)
+        recorder = MediaRecorder("hw:3,0", format="alsa")
+        recorder.addTrack(self.track)
+        await recorder.start()
         while self.track.readyState != 'ended':
-            next_frame = await self.track.recv()
-            audio_data = av.AudioFrame.to_ndarray(next_frame).tobytes()
-            stream.write(audio_data)
+            await asyncio.sleep(1)
+        await recorder.stop()
