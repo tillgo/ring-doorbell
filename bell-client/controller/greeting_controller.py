84cc8059aa146375b870491e83e424e3b80198e3
Michael Delser <michadelser@gmail.com>
2024-06-02 17:55:02 +0100

    select which user to call

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -27,9 +27,9 @@ class GreetingController:
         for user in self.visitorData.possibleUsers:
             self.ui.userList.addItem(QStandardItem(user.username))
         # Select the first possible user
-        self.ui.userList.currentRow(0)
         self.selectedCameraUserId = self.visitorData.possibleUsers[self.ui.userList.currentRow].id
         self.ui.userList.currentItemChanged.connect(self.on_current_changed)
+        self.ui.userList.currentRow(0)
 
         self.ui.page_stacked_widget.setCurrentWidget(self.ui.greeting_page)
 
