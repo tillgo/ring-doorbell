8c4959241ad6f9e32a9a60613e341ddd16a7cc86
Siggi <simeon-holzapfel@gmx.de>
2024-06-02 19:27:52 +0200

    delete unused print

--- bell-client/controller/main_controller.py
+++ bell-client/controller/main_controller.py
@@ -12,16 +12,16 @@ class MainController(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.ui = main_window.Ui_MainWindow()
-        self.ui.setupUi(self)
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.greetingController = None
         self.show()
         self.start_app()
 
     def start_app(self):
-        self.greetingController = GreetingController(self.ui, self)
-        self.ui.page_stacked_widget.setCurrentWidget(self.ui.ring_page)
+        ui = main_window.Ui_MainWindow()
+        ui.setupUi(self)
+        self.greetingController = GreetingController(ui, self)
+        ui.page_stacked_widget.setCurrentWidget(ui.ring_page)
         # start waiting for nfc id in new thread
         t1 = threading.Thread(target=wait_for_nfc_id, args=(self.handle_nfc_id_found,))
         t1.start()
