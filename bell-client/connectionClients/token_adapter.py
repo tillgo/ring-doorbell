0b7e0730accd4d5ade8e81139f8bc3493fa4f3c1
Siggi <simeon-holzapfel@gmx.de>
2024-06-02 12:56:43 +0200

    make changes to token adapter

--- bell-client/connectionClients/token_adapter.py
+++ bell-client/connectionClients/token_adapter.py
@@ -3,18 +3,17 @@ from requests.adapters import HTTPAdapter
 
 class TokenAdapter(HTTPAdapter):
     """
-    Custom Transport Adapter that retries failed requests.
+    Custom Transport Adapter that appends JWT to all requests.
     """
 
-    def __init__(self):
+    def __init__(self, client):
         super().__init__()
+        self.client = client
 
     def send(self, request, stream=False, timeout=None, verify=True, cert=None, proxies=None):
         print("token adapter opened")
-        from connectionClients.http_client import HttpClient
-        client = HttpClient()
         # If not auth route, append jwts
         if not ("/api/auth" in request.url):
-            token = client.get_token()
+            token = self.client.get_token()
             print(token)
             request.headers['authorization'] = f"Bearer {token}"
