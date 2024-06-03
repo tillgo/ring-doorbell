f62daeed78295286afaab640d3c0ca90f86e3229
Siggi <simeon-holzapfel@gmx.de>
2024-06-03 15:38:56 +0200

    handle connection closed

--- web/src/base/components/VideoCallControls.tsx
+++ web/src/base/components/VideoCallControls.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/lib/components/ui/button.tsx'
-import { Phone, Video, VideoOff } from 'lucide-react'
+import { Phone } from 'lucide-react'
 import { ChangeEvent } from 'react'
 
 export type CallControllerControllsProps = {
@@ -14,30 +14,10 @@ export type CallControllerControllsProps = {
 }
 
 export const VideoCallControls = (props: CallControllerControllsProps) => {
-    const { isVideoOn, onEnableVideo, onDisableVideo, onEndCall } = props
+    const { onEndCall } = props
 
     return (
         <div className={'flex justify-center gap-6 p-4'}>
-            {isVideoOn ? (
-                <Button
-                    className={'h-14 w-14'}
-                    variant={'outline'}
-                    size={'icon'}
-                    onClick={onDisableVideo}
-                >
-                    <VideoOff className={'h-8 w-8'} />
-                </Button>
-            ) : (
-                <Button
-                    className={'h-14 w-14'}
-                    size={'icon'}
-                    variant={'outline'}
-                    onClick={onEnableVideo}
-                >
-                    <Video className={'h-8 w-8'} />
-                </Button>
-            )}
-
             <Button
                 variant={'destructive'}
                 size={'icon'}
