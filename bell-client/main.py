4f0d5d69690cc9d765700214701ab23c321d5689
Siggi <simeon-holzapfel@gmx.de>
2024-06-05 14:13:55 +0200

    remove nfc icon

--- bell-client/main.py
+++ bell-client/main.py
@@ -6,30 +6,6 @@ from qasync import QEventLoop
 
 from controller.main_controller import MainController
 
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Raspberry Pi Camera Stream")
-        self.setGeometry(100, 100, 480, 250)
-
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-        layout = QVBoxLayout(self.central_widget)
-
-        self.video_label = QLabel(self)
-        self.video_label.setWindowFlag(Qt.WindowType.FramelessWindowHint)
-        layout.addWidget(self.video_label)
-
-    def update_frame(self, pixmap):
-        print("Pixmap")
-        print(pixmap)
-        if pixmap is not None:
-            self.video_label.setPixmap(pixmap)
-            self.video_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-main_window = None
-
 if __name__ == "__main__":
     app = QApplication([])
     app.setStyle('Fusion')
