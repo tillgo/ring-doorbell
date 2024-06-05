043626005f2c9bb480a629df84cae8eb2763cf41
Siggi <simeon-holzapfel@gmx.de>
2024-06-05 17:26:26 +0200

    Add debug string

--- bell-client/controller/greeting_controller.py
+++ bell-client/controller/greeting_controller.py
@@ -32,7 +32,7 @@ class GreetingController:
         # ("142b3e7f-1562-4335-9653-d98eac0c6f73")
         #self.call_user_controller.call_user(self.selectedCameraUserId)
         index = self.ui.userList.currentRow()
-        print("Current Index: " + index)
+        print("Current Index: " + str(index))
         user = self.visitorData.possibleUsers[index]
         print("Username: " + user.username)
         print("ID: " + user.id)
